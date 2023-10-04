670779dfa0cccfcd74684d501eeb65a7752f13c8
Palak <palak.desai4501@gmail.com>
2023-10-04 11:29:26 +0530

    Feature: Added post method to create and save book

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,48 @@
- import express, { request, response } from "express";
- import { PORT, mongoDBURL } from "./config.js";
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
- const app = express();
+const app = express();
 
- app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(234).send('welcome to MERN Stack Tutorial')
- });
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome to MERN Stack Tutorial");
+});
 
- 
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
 
- mongoose.connect(mongoDBURL).then(() => {
-    console.log('App Connected to database');
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App Connected to database");
     app.listen(PORT, () => {
-        console.log(`App is listening to port: ${PORT}`);
-     });
- }).catch((error) => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
     console.log(error);
- });
+  });
