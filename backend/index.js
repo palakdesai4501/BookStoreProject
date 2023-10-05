81bebbb73cbf5916465bdf998b70104d1b29aabd
Palak <palak.desai4501@gmail.com>
2023-10-05 15:03:26 +0530

    Feature: Added CORS Policy Dependency and Checked It

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 
 const app = express();
 
@@ -10,6 +11,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS Policy
+//Option 1: Allow All Origins With Default of cors(*)
+// app.use(cors());
+
+//Option 2: Allow Custom Origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
