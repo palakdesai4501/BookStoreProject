9a0d7e135e2e8ab5c4d41c912d63b3060cc0fc57
Palak <palak.desai4501@gmail.com>
2023-10-05 20:33:21 +0530

    Feature: Called API via useEffect Hook

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,20 @@ import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios.get('http://localhost:5555/books')
+    .then((response) => {
+      setBooks(response.data.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
+  
   return (
     <div>
       Home
