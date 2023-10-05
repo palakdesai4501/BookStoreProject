8a620647e7639910f4c5151939e697e2a7e7cb54
Palak <palak.desai4501@gmail.com>
2023-10-05 10:43:50 +0530

    Feature: Format Document

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
-import express, { request, response } from "express";
+import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import bodyParser from "body-parser"
-import booksRoute from './routes/booksRoute.js'
-import {Book} from './models/bookModel.js';
-
+import bodyParser from "body-parser";
+import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Middleware for parsing request body
 app.use(express.json());
 
@@ -18,21 +16,21 @@ app.get("/", (request, response) => {
   return response.status(234).send("welcome to MERN Stack Tutorial");
 });
 
-app.use('/books', booksRoute);
+app.use("/books", booksRoute);
 
-mongoose.connect('mongodb://127.0.0.1:27017/book_store', {
+mongoose.connect("mongodb://127.0.0.1:27017/book_store", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
 });
 
-mongoose.connection.on('error', (err) => {
-  console.error('Error connecting to MongoDB:', err);
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to MongoDB:", err);
 });
 
 app.listen(PORT, () => {
-  console.log('Server is listening....');
+  console.log("Server is listening....");
 });
