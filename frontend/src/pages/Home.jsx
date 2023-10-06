f1274dc2c177249851caef08f9de8521fc010ddf
Palak <palak.desai4501@gmail.com>
2023-10-06 10:00:17 +0530

    Feature: Added UI and Implemented Tailwind CSS For Styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,8 +22,33 @@ const Home = () => {
   }, []);
   
   return (
-    <div>
-      Home
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Books List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-sky-800 text-4xl'/>
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className='w-full border-separate border-spacing-2'>
+          <thead>
+            <tr>
+              <th className='border border-slate-600 rounded-md'>No</th>
+              <th className='border border-slate-600 rounded-md'>Title</th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
+              <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
+              <th className='border border-slate-600 rounded-md'>Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+
+        </table>
+
+      )}
     </div>
   )
 }
