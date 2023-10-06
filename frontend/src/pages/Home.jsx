6d67fec109652fc8c2c009b64f5bdbabd8de5070
Palak <palak.desai4501@gmail.com>
2023-10-06 10:36:53 +0530

    Feature: Added Table Rows And Icons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Spinner from '../components/spinner';
-import { Link } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
@@ -18,6 +18,7 @@ const Home = () => {
     })
     .catch((error) => {
       console.log(error);
+      setLoading(false);
     });
   }, []);
   
@@ -43,7 +44,35 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-
+            {books.map((book, index) => (
+              <tr key={book._id} className='h-8'>
+                <td className='border border-slate-700 rounded-md text-center'>
+                  {index+1}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center'>
+                  {book.title}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                  {book.author}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                  {book.publishYear}
+                </td>
+                <td className='border border-slate-700 rounded-md text-center'>
+                  <div className='flex justify-center gap-x-4'>
+                    <Link to={`/books/details/${book._id}`}>
+                      <BsInfoCircle className='text-2xl text-green-800'/>
+                    </Link>
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit className='text-2xl text-yellow-600'/>
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                      <MdOutlineDelete className='text-2xl text-red-600'/>
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
 
         </table>
