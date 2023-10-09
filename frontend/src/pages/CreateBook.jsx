a0f04f6d89dc1a7cfd8aa3174f84a38478d432da
Palak <palak.desai4501@gmail.com>
2023-10-09 10:48:52 +0530

    Feature: Added useNavigate Hook And Created Function To Handle Book

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../component/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const CreateBook = () => {
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [ publishYear, setPublishYear ] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+    .post('http://localhost:5555/books', data)
+    .then(() => {
+      setLoading(false);
+      navigate('/');
+    })
+    .catch((error) => {
+      setLoading(false);
+      alert('An error happended. Please Chack console');
+      console.log(error);
+    });
+  };
+
   return (
     <div>
       CreateBook
