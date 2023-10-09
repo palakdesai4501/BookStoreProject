aeb73b6136c24667c29ca4146fd36f8749c58e0d
Palak <palak.desai4501@gmail.com>
2023-10-09 13:57:59 +0530

    Feature: Added Function to handle delete book featurw

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -5,6 +5,23 @@ import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom'
 
 const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios.delete(`http://localhost:5555/books/${id}`)
+    .then(() => {
+      setLoading(false);
+      navigate('/');
+    })
+    .catch((error) => {
+      setLoading(false);
+      alert('An error Happened. Please check Console');
+      console.log(error);
+    })
+  }
   return (
     <div>
       DeleteBook
