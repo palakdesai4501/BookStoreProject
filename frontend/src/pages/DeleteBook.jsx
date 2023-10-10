95c87e96704c5826a55946b2179d7e2c9a11d206
Palak <palak.desai4501@gmail.com>
2023-10-10 13:35:19 +0530

    Feature: Format Document

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import BackButton from '../components/BackButton';
-import Spinner from '../components/Spinner';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom'
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DeleteBook = () => {
   const [loading, setLoading] = useState(false);
@@ -11,30 +11,35 @@ const DeleteBook = () => {
 
   const handleDeleteBook = () => {
     setLoading(true);
-    axios.delete(`http://localhost:5555/books/${id}`)
-    .then(() => {
-      setLoading(false);
-      navigate('/');
-    })
-    .catch((error) => {
-      setLoading(false);
-      alert('An error Happened. Please check Console');
-      console.log(error);
-    })
-  }
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error Happened. Please check Console");
+        console.log(error);
+      });
+  };
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <BackButton />
-      <h1 className='text-3xl my-4'>Delete Book</h1>
-      {loading ? <Spinner /> : ''}
-      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
-        <h3 className='text-2xl'>Are You Sure You Want To Delete This book</h3>
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are You Sure You Want To Delete This book</h3>
 
-        <button className='p-4 bg-red-600 text-white m-8 w-full'
-        onClick={handleDeleteBook}>Yes, Delete it</button>
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DeleteBook
+export default DeleteBook;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Spinner from '../components/Spinner';
-import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
-import BooksCard from '../components/home/BooksCard';
-import BooksTable from '../components/home/BooksTable';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import BooksCard from "../components/home/BooksCard";
+import BooksTable from "../components/home/BooksTable";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
-    axios.get('http://localhost:5555/books')
+    axios
+      .get("http://localhost:5555/books")
       .then((response) => {
-        console.log("RESponse",response);
+        console.log("RESponse", response);
         setBooks(response.data.data);
         setLoading(false);
       })
@@ -26,21 +27,17 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='p-4'>
-      <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Books List</h1>
-        <Link to='/books/create'>
-          <MdOutlineAddBox className='text-sky-800 text-4xl' />
+    <div className="p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl my-8">Books List</h1>
+        <Link to="/books/create">
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <BooksTable books={books}/>
-      )}
+      {loading ? <Spinner /> : <BooksTable books={books} />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
-Home
+export default Home;
+Home;
