c45e011c11b3f6e76c08e9f0dcbfc308717ac867
Palak <palak.desai4501@gmail.com>
2023-10-10 18:15:15 +0530

    Feature: Added Item Prop

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -11,7 +11,7 @@ const BooksCard = ({ books }) => {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {books.map((item) => (
-        <BookSingleCard key={item._id}/>
+        <BookSingleCard key={item._id} book={item}/>
       ))}
     </div>
   );
