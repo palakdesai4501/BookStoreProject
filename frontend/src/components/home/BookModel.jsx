404d66bd7134417d6f0577d1f9f4ab4099cbd1d6
Palak <palak.desai4501@gmail.com>
2023-10-10 18:25:34 +0530

    Feature: Added BookModel Component

---
+++ frontend/src/components/home/BookModel.jsx
@@ -0,0 +1,16 @@
+import { AiOutlineClose } from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
+import { PiBookOpenTextLight } from "react-icons/pi";
+
+const BookModel = ({ book, onClose }) => {
+  return (
+    <div
+      className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
+      onClick={onClose}
+    >
+      BookModel
+    </div>
+  );
+};
+
+export default BookModel;
